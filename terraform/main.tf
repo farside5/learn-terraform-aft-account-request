d56216ddd8da6bdd64ed87cbe47c7cacd9e1480b
Joe Zhou <jzhou571@gmail.com>
2022-02-11 16:11:03 +0000

    delete aft_dev1, create aft_dev2

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,12 +1,40 @@
-module "sandbox" {
+# module "sandbox" {
+#   source = "./modules/aft-account-request"
+
+#   control_tower_parameters = {
+#     AccountEmail              = "jozhou+ctv3_aft1@amazon.com"
+#     AccountName               = "aft-dev1"
+#     ManagedOrganizationalUnit = "AFT"
+#     SSOUserEmail              = "jozhou+ctv3_aft1@amazon.com"
+#     SSOUserFirstName          = "AftDev1"
+#     SSOUserLastName           = "Zhou"
+#   }
+
+#   account_tags = {
+#     "Learn Tutorial" = "AFT"
+#   }
+
+#   change_management_parameters = {
+#     change_requested_by = "HashiCorp Learn"
+#     change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
+#   }
+
+#   custom_fields = {
+#     group = "non-prod"
+#   }
+
+#   account_customizations_name = "aft-dev1"
+# }
+
+module "aft-dev2" {
   source = "./modules/aft-account-request"
 
   control_tower_parameters = {
-    AccountEmail              = "jozhou+ctv3_aft1@amazon.com"
-    AccountName               = "aft-dev1"
+    AccountEmail              = "jozhou+ctv3_aftdev2@amazon.com"
+    AccountName               = "aft-dev2"
     ManagedOrganizationalUnit = "AFT"
-    SSOUserEmail              = "jozhou+ctv3_aft1@amazon.com"
-    SSOUserFirstName          = "AftDev1"
+    SSOUserEmail              = "jozhou+ctv3_aftdev2@amazon.com"
+    SSOUserFirstName          = "AftDev2"
     SSOUserLastName           = "Zhou"
   }
 
@@ -23,5 +51,5 @@ module "sandbox" {
     group = "non-prod"
   }
 
-  account_customizations_name = "aft-dev1"
+  account_customizations_name = "aft-dev2"
 }
